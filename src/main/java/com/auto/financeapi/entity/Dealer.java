cb3d2d389b3a31a83382eeab0fe822fd3f0f3f7c
VamsiThorthi <vamsithorthi@gmail.com>
2022-09-08 10:56:55 +0530

    added dealer name in master

--- src/main/java/com/auto/financeapi/entity/Dealer.java
+++ src/main/java/com/auto/financeapi/entity/Dealer.java
@@ -20,6 +20,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * 
+ * @author saivamsit
+ *
+ */
 @Entity
 @Table(name = "dealers")
 @AllArgsConstructor
